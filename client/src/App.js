1a5ad96dfe989fc07dc9fe8808badb40d3947fc4
PaulS <psoliz333@gmail.com>
2022-06-23 17:14:33 -0500

    created routes for all pages and added friends list and able to view comments and reactions from profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -18,13 +24,54 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Routes>
+              {/* home */}
+              <Route
+                path='/'
+                element={<Home />} />
+
+              {/* login */}
+              <Route
+                path='/login'
+                element={<Login />}
+              />
+
+              {/* signup */}
+              <Route
+                path='/signup'
+                element={<Signup />}
+              />
+
+              {/* profile */}
+              <Route
+                path="/profile"
+                element={<Profile />}
+              />
+
+              {/* thought */}
+              <Route path="/profile">
+                <Route path=":username" element={<Profile />} />
+                <Route path="" element={<Profile />} />
+              </Route>
+              <Route
+                path="/thought/:id"
+                element={<SingleThought />}
+              />
+
+              <Route
+                path="*"
+                element={<NoMatch />}
+              />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
+
     </ApolloProvider>
 
   );
